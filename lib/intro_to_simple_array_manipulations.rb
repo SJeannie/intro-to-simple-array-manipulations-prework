3b93c4c065064e131d8dad522d5de2aa35fd95a7
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-13 20:17:26 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,67 @@
+def using_push(countries_in_western_africa,next_country)
+  
+  updated_array = countries_in_western_africa.push(next_country)
+  
+  return updated_array
+
+end
+
+def using_unshift
+  
+  
+  
+end
+
+def using_pop 
+  
+  
+  
+end
+
+def pop_with_args
+  
+  
+  
+end
+
+def using_shift
+  
+  
+  
+end
+
+def using_concat
+  
+  
+  
+end
+
+def using_insert
+  
+  
+  
+end
+
+def using_uniq
+  
+  
+  
+end
+
+def using_flatten
+  
+  
+  
+end
+
+def using_delete
+  
+  
+  
+end
+
+def using_delete_at
+  
+  
+  
+end
